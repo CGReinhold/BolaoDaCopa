f4348208e48e080bb3750fd8b98c5e1205ff2c69
Anderson Rissardi <anderson.rissardi@hotmail.com>
2018-05-30 17:56:16 -0300

    a

--- server/functions/index.js
+++ server/functions/index.js
@@ -10,6 +10,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+admin.initializeApp();
+
 exports.calculaPartida = functions.database.ref('/matches/{pushId}/teste').onCreate((snapshot, context) => {
   const original = snapshot.val();
   console.log('Original: ', JSON.stringify(original)); 
@@ -17,3 +19,17 @@ exports.calculaPartida = functions.database.ref('/matches/{pushId}/teste').onCre
   const uppercase = original.toUpperCase();
   return snapshot.ref.parent.child('uppercase').set(uppercase);
 });
+
+exports.calculaPontuacaoUsuariosDoJogo =  functions.database.ref('/matches/{partidaID}/finished').onUpdate((snapshot, context) => {
+    if(snapshot.before.val() === false && snapshot.after.val() === true) {
+      //snapshot trás apenas o valor do finished, para trazer os outros valores é preciso carregar o filtro utilizando a propriedade 'name' da partida.
+      // Essa propriedade é o identificador das apostas do usuário
+      //Através dessa propriedade é possível carregar as apostas de cada usuário
+      
+      admin.database().ref('/users').once('value', usuario => {
+         
+      });
+      
+    }
+});
+
