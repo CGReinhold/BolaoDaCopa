4215aaf593a3803e14b9e6e806c63b08c64aa84d
Anderson Rissardi <anderson.rissardi@hotmail.com>
2018-05-31 16:25:58 -0300

    function testada, só melhorar ela...

--- server/functions/index.js
+++ server/functions/index.js
@@ -10,26 +10,38 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
 
-exports.calculaPartida = functions.database.ref('/matches/{pushId}/teste').onCreate((snapshot, context) => {
-  const original = snapshot.val();
-  console.log('Original: ', JSON.stringify(original)); 
-  console.log('Uppercasing', context.params.pushId, original);
-  const uppercase = original.toUpperCase();
-  return snapshot.ref.parent.child('uppercase').set(uppercase);
-});
+//admin.initializeApp();
 
-exports.calculaPontuacaoUsuariosDoJogo =  functions.database.ref('/matches/{partidaID}/finished').onUpdate((snapshot, context) => {
-    if(snapshot.before.val() === false && snapshot.after.val() === true) {
+// exports.calculaPartida = functions.database.ref('/matches/{pushId}/teste').onCreate((snapshot, context) => {
+//   const original = snapshot.val();
+//   console.log('Original: ', JSON.stringify(original)); 
+//   console.log('Uppercasing', context.params.pushId, original);
+//   const uppercase = original.toUpperCase();
+//   return snapshot.ref.parent.child('uppercase').set(uppercase);
+// });
+
+exports.calculaPontuacaoUsuariosDoJogo = functions.database.ref('/matches/{partidaID}/finished').onUpdate((snapshot, context) => {
+    //if(snapshot.before.val() === false && snapshot.after.val() === true) {
       //snapshot trás apenas o valor do finished, para trazer os outros valores é preciso carregar o filtro utilizando a propriedade 'name' da partida.
       // Essa propriedade é o identificador das apostas do usuário
       //Através dessa propriedade é possível carregar as apostas de cada usuário
       
-      admin.database().ref('/users').once('value', usuario => {
-         
+      console.log('>> snapshot: ', JSON.stringify(snapshot.after.val()));
+      console.log('>> partidaID: ', JSON.stringify(context.params.partidaID))
+      console.log('>> parent: ', admin.database().ref('/matches/' + context.params.partidaID).toJSON());
+
+      admin.database().ref('/users').once('value', usuariosBase => {
+         const usuarios = usuariosBase.val();
+         console.log('>>');
+         console.log('>>');
+         console.log('>> Usuarios - ', JSON.stringify(usuarios));
+        //  usuarios.forEach((element, index) => {
+
+        //  });
       });
       
-    }
+    //}
 });
 
